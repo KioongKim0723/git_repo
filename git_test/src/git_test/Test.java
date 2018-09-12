c564d3d66b6a0567d9455ffeb089a5efb7527ff0
Honggildong <Honggildong@naver.com>
2018-09-12 11:05:44 +0900

    First Commit

--- git_test/src/git_test/Test.java
+++ git_test/src/git_test/Test.java
@@ -3,6 +3,6 @@ package git_test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello");
+		System.out.println("Hello1");
 	}
 }
